a563ef4600782bca38eb6e037a1ab10db23dcc54
yone-coder <147109802+yone-coder@users.noreply.github.com>
2025-05-23 19:20:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import express from 'express';
 import fetch from 'node-fetch';
-import cors from 'cors';
 import 'dotenv/config';
 
 const app = express();
 
-// Middleware
-app.use(cors());
+// CORS middleware
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
+// Body parsing middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
